799c005dced1f45b203a4f866cc56ffeafed56d5
Stelios Vakoufis <stelios.vakoufis@gmail.com>
2023-03-04 01:32:35 +0200

    Add new models
    [User, UserRole, Book, BookAuthor, BookCategory, BookPhoto, AddressInformation, BookOrder, Order]
    
    Added new User, UserRole, Book, BookAuthor, BookCategory, BookPhoto, AddressInformation, BookOrder and Order models to the project.

---
+++ MindTheCodeApp/Models/OrderModels/AddressInformation.cs
@@ -0,0 +1,30 @@
+﻿using DBModelExercise.Data.Models.Auth;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Diagnostics.CodeAnalysis;
+
+namespace DBModelExercise.Data.Models.Orders
+{
+    [Table("Address_Information")]
+    public class AddressInformation
+    {
+        [Key, Column("address_information_id"), NotNull, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int AddressInformationId { get; set; }
+
+        [Required, Column("street_address"), NotNull]
+        public string? StreetAddress { get; set; }
+
+        [Required, Column("city"), NotNull]
+        public string? City { get; set; }
+
+        [Required, Column("postal_code"), NotNull]
+        public string? PostalCode { get; set; }
+
+        [Required, Column("country"), NotNull]
+        public string? Country { get; set; }
+
+        public ICollection<User>? Users { get; set; }
+
+        public ICollection<Order>? Orders { get; set; }
+    }
+}
---
+++ MindTheCodeApp/Models/OrderModels/Order.cs
@@ -0,0 +1,39 @@
+﻿using DBModelExercise.Data.Models.Auth;
+using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Diagnostics.CodeAnalysis;
+
+namespace DBModelExercise.Data.Models.Orders
+{
+    [Table("Orders")]
+    public class OrderModel
+    {
+        [Key, Column("order_id"), NotNull, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int OrderId { get; set; }
+
+        [Required, Column("user_id"), ForeignKey("user_id"), NotNull]
+        public User? User { get; set; }
+
+        [Column("is_fulfilled"), AllowNull]
+        public bool Fulfilled { get; set; }
+
+        [Column("is_active"), AllowNull]
+        public bool Active { get; set; }
+
+        [Column("is_canceled"), AllowNull]
+        public bool Canceled { get; set; }
+
+        [Required, Column("address_information_id"), ForeignKey("address_information_id"), NotNull]
+        public AddressInformation? AddressInformation { get; set; }
+
+        [Required, Column("cost"), NotNull]
+        public double Cost { get; set; }
+
+        [Column("date_created"), NotNull]
+        public DateTime DateCreated { get; set; } = DateTime.Now;
+
+        public ICollection<BookOrder>? BookOrder { get; set; }
+
+    }
+}
