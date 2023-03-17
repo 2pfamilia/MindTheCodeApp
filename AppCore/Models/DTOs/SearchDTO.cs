a4c6075c4b682605693a27d97541f2c217cc5036
Stelios Vakoufis <stelios.vakoufis@gmail.com>
2023-03-17 23:53:12 +0200

    Create SearchDTO

---
+++ AppCore/Models/DTOs/SearchDTO.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppCore.Models.DTOs
+{
+    public class SearchDTO
+    {
+        public string? SearchTerm { get; set; }
+        public int[]? Categories { get; set; }
+        public int[]? Authors { get; set; }
+
+        //public int? PageIndex { get; set; }
+        //public int? PageSize { get; set; }
+        //public int? PageCount { get; set; }
+    }
+}
