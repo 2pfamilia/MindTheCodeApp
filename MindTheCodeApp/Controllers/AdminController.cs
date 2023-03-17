9ebb696e370cde9c8b747735ec23bb953dc806bf
Peter Papadopoulos <pxpapas@gmail.com>
2023-03-17 15:09:22 +0200

    changes

---
+++ MindTheCodeApp/Controllers/AdminController.cs
@@ -0,0 +1,23 @@
+﻿using AppCore.IRepositories;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MindTheCodeApp.Controllers
+{
+    public class AdminController : Controller
+    {
+        private readonly ILogger<AdminController> _logger;
+        private readonly IBookRepository _bookRepository;
+
+        public AdminController(ILogger<AdminController> logger, IBookRepository bookRepository)
+        {
+            _bookRepository = bookRepository;
+            _logger = logger;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var books = await _bookRepository.GetAllBooks();
+
+            return View(books);
+        }
+    }
+}
