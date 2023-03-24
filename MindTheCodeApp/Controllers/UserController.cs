5db17778e3ee0490d37e82c5788033add9f8bacd
Stelios Vakoufis <stelios.vakoufis@gmail.com>
2023-03-25 00:21:07 +0200

    Move AuthController actions to UserController

--- MindTheCodeApp/Controllers/UserController.cs
+++ MindTheCodeApp/Controllers/UserController.cs
@@ -3,17 +3,24 @@ using AppCore.Models.DTOs;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using MindTheCodeApp.Services.IServices;
+using AppCore.Models.AuthModels;
+using Infrastructure.Data;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.EntityFrameworkCore;
 
 namespace MindTheCodeApp.Controllers;
 
 [Route("/Account/")]
 public class UserController : Controller
 {
+    private readonly ApplicationDbContext _context;
     private readonly IUserService _userService;
 
-    public UserController(IUserService userService)
+    public UserController(IUserService userService, ApplicationDbContext context)
     {
         _userService = userService;
+        _context = context;
     }
 
     [HttpGet("Info")]
@@ -26,7 +33,7 @@ public class UserController : Controller
 
         return View("/Views/MyAccount/MyAccount.cshtml", dto);
     }
-    
+
     [HttpPost("ChangeInfo")]
     [Authorize(Roles = "reuser, admin")]
     public async Task<IActionResult> InfoUpdate([FromForm] UserInfoDTO dto)
@@ -34,10 +41,10 @@ public class UserController : Controller
         var userId = int.Parse(User.FindFirst(ClaimTypes.NameIdentifier)?.Value ?? "-1");
 
         await _userService.UpdateUserInfo(dto, userId);
-        
+
         return RedirectToAction("InfoView", "User");
     }
-    
+
     [HttpPost("ChangePassword")]
     [Authorize(Roles = "reuser, admin")]
     public async Task<IActionResult> UpdateUserPassword([FromForm] UserChangePasswordDTO dto)
@@ -45,10 +52,10 @@ public class UserController : Controller
         var userId = int.Parse(User.FindFirst(ClaimTypes.NameIdentifier)?.Value ?? "-1");
 
         await _userService.UpdateUserPassword(dto, userId);
-        
+
         return RedirectToAction("InfoView", "User");
     }
-    
+
     [HttpGet("Login")]
     [AllowAnonymous]
     // Test Controller for Login.
@@ -58,5 +65,98 @@ public class UserController : Controller
             return RedirectToAction("Index", "Home");
 
         return View("/Views/Auth/Login.cshtml");
+    }
+
+    [HttpPost("Login")]
+    [AllowAnonymous]
+    public async Task<IActionResult> Login([FromForm] User user)
+    {
+        if (User.Identity!.IsAuthenticated)
+            return RedirectToAction("Index", "Home");
+        
+        if (user is null || user.Email is null || user.Password is null)
+            return BadRequest();
+
+        // Check if user exists in database with the correct password.
+        try
+        {
+            user = _context.UserEntity
+                .Include(u => u.Role)
+                .Single(u =>
+                    u.Email.Equals(user.Email) &&
+                    u.Password.Equals(user.Password)
+                );
+        }
+        catch (Exception)
+        {
+            return NotFound();
+        }
+
+        // Create claims for the user
+        var claims = new List<Claim>
+        {
+            new Claim(ClaimTypes.NameIdentifier, user.UserId.ToString()),
+            new Claim(ClaimTypes.Role, user.Role.Code),
+            new Claim(ClaimTypes.Email, user.Email),
+        };
+
+        var claimsIdentity = new ClaimsIdentity(claims, CookieAuthenticationDefaults.AuthenticationScheme);
+        var principal = new ClaimsPrincipal(claimsIdentity);
+
+        await HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, principal);
+
+        return RedirectToAction("Index", "Home");
+    }
+
+    [HttpGet("Logout")]
+    [Authorize(Roles = "reuser, admin")]
+    public async Task<IActionResult> Logout()
+    {
+        await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+
+        return RedirectToAction("Index", "Home");
+    }
+
+    [HttpGet("Register")]
+    [AllowAnonymous]
+    public async Task<IActionResult> RegisterView()
+    {
+        if (User.Identity!.IsAuthenticated)
+            return RedirectToAction("Index", "Home");
+        
+        return View("/Views/Auth/Register.cshtml");
+    }
+
+    [HttpPost("Register")]
+    [AllowAnonymous]
+    public async Task<IActionResult> Register([FromForm] RegisterDTO registerDTO)
+    {
+        if (User.Identity!.IsAuthenticated)
+            return RedirectToAction("Index", "Home");
+        
+        User user = new User();
+
+        //user.Email = registerDTO.Email;
+
+        if (registerDTO.Email is null || registerDTO.FirstName is null || registerDTO.LastName is null
+            || registerDTO.City is null || registerDTO.StreetAddress is null || registerDTO.Country is null)
+        {
+            TempData["msg"] = "<script>alert('All Fields are Obligatory');</script>";
+
+            return View("/Views/Auth/Register.cshtml");
+        }
+
+        var userCreated = await _userService.CreateUser(registerDTO);
+
+        if (userCreated)
+        {
+            TempData["msg"] = "<script>alert('User Created');</script>";
+        }
+        else
+        {
+            TempData["msg"] = "<script>alert('User Already Exists');</script>";
+        }
+
+        return View("/Views/Auth/Register.cshtml");
     }
 }
