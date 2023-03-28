2b5f94b9e87408cb505b37988bd8784308a67596
Stelios Vakoufis <stelios.vakoufis@gmail.com>
2023-03-28 20:22:52 +0300

    Fix Photo model not being included

--- Infrastructure/Data/Repositories/BookRepository.cs
+++ Infrastructure/Data/Repositories/BookRepository.cs
@@ -1,7 +1,6 @@
 using Microsoft.EntityFrameworkCore;
 using AppCore.IRepositories;
 using AppCore.Models.BookModels;
-using AppCore.Models.AuthModels;
 
 namespace Infrastructure.Data.Repositories
 {
@@ -16,26 +15,26 @@ namespace Infrastructure.Data.Repositories
 
         public async Task<List<Book>> GetAllBooks()
         {
-            var books = await _context.BookEntity.ToListAsync();
+            var books = await _context.BookEntity.Include(b => b.Photo).ToListAsync();
             return books;
         }
 
         public async Task<List<Book>> GetBestSellers()
         {
-            var bestSellers = await _context.BookEntity.Take(4).ToListAsync();
+            var bestSellers = await _context.BookEntity.Include(b => b.Photo).Take(4).ToListAsync();
             return bestSellers;
         }
 
         public async Task<List<BookAuthor>> GetAllAuthors()
         {
             //george
-            var allAuthors = await _context.BookAuthorEntity.Take(5).ToListAsync();
+            var allAuthors = await _context.BookAuthorEntity.Include(ba => ba.Photo).Take(5).ToListAsync();
             return allAuthors;
         }
 
         public async Task<List<BookAuthor>> GetBestSellingAuthors()
         {
-            var bestSelling = await _context.BookAuthorEntity.Take(5).ToListAsync();
+            var bestSelling = await _context.BookAuthorEntity.Include(ba => ba.Photo).Take(5).ToListAsync();
             return bestSelling;
         }
 
@@ -90,7 +89,8 @@ namespace Infrastructure.Data.Repositories
         public async Task<List<Book>> GetNewArrivals()
         {
             //get the first 5 books that are newer in the library
-            var newArrivals = await _context.BookEntity.OrderByDescending(mybook => mybook.DateCreated).Take(4)
+            var newArrivals = await _context.BookEntity.Include(b => b.Photo)
+                .OrderByDescending(mybook => mybook.DateCreated).Take(4)
                 .ToListAsync();
             return newArrivals;
         }
@@ -123,8 +123,9 @@ namespace Infrastructure.Data.Repositories
 
         public Book GetBookInfoById(int bookId)
         {
-            Book book = _context.BookEntity.Include(x=>x.Author).Include(x=>x.Category).Single(bookById => bookById.BookId == bookId);
-           
+            Book book = _context.BookEntity.Include(x => x.Author).Include(x => x.Category)
+                .Single(bookById => bookById.BookId == bookId);
+
             return book;
         }
     }
